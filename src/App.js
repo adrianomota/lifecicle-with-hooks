2590fb7795bd667c4a30435130b29cfac476d1d6
Adriano Mota <adrianowsh@hotmail.com>
2019-07-22 21:21:48 -0300

    ïnitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+  const [techs, setTech] = useState(['ReactJS', 'Node.js', 'React native']);
+  const [newTech, setNewTech] = useState('');
+
+  // a cada mudanca de estado esta funcao é criada novamente
+  // dispercando processamento
+  // para isso usamos o hook useCallback
+  // desta forma a funcao so sera recriada quando os valores mudarem
+  const handleAdd = useCallback(() => {
+    setTech([...techs, newTech]);
+    setNewTech('');
+  }, [newTech, techs]);
+
+  // executado uma unica vez, pois nao monitora nenhuma variavel
+  // equivalente ao ComponentDidMount
+  useEffect(() => {
+    const techExist = localStorage.getItem('techs');
+
+    if (techExist) setTech(JSON.parse(techExist));
+
+    // return () => {} execurtado quando o comonente deixae de existir
+  }, []);
+
+  // será executado uma unica vez,
+  // depois só será executado quando a lista for modificada
+  // Equivalente ao componente ComponentDidUpdate
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+
+    // return () => {} execurtado quando o comonente deixae de existir
+  }, [techs]);
+
+  // usado para realizar calculos uma unica vez antes
+  // do componente ser criado
+  const techSize = useMemo(() => techs.length, [techs]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <ul>
+          {techs.map(tech => (
+            <li key={Math.random()}>{tech}</li>
+          ))}
+        </ul>
+        <strong>Voce tem {techSize} tecnologias</strong>
+        <input
+          type="text"
+          value={newTech}
+          onChange={e => setNewTech(e.target.value)}
+        />
+        <button type="button" onClick={handleAdd}>
+          Adicionar
+        </button>
+      </div>
+    </>
   );
 }
 
